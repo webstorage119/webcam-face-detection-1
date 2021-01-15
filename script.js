c90dc7c1ec844b7d7d1b0d2346a901a89cf6b9f6
Jordan Long <jblong27@gmail.com>
2021-01-15 13:15:57 -0600

    fix console error where faceapi was not defined

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tnyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68net.loadFromUri('/models'),
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo)
@@ -11,8 +11,10 @@ function startVideo() {
   navigator.getUserMedia(
     { video: {} }, 
     stream => video.srcObject = stream,
-    err => console.(err)
+    err => console.error(err)
   )
 }
 
-startVideo()
+video.addEventListener('play', () => {
+  console.log('hello')
+})
